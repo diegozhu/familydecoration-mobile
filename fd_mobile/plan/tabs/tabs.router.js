76a8b6035a19fab5392d3b9d452f33cdbd2f8780
alex123bob <alex123bob@126.com>
2017-03-19 22:55:50 +0800

    海洋答疑一下

--- fd_mobile/plan/tabs/tabs.router.js
+++ fd_mobile/plan/tabs/tabs.router.js
@@ -10,7 +10,7 @@
       .state({
         name: 'home.plan',
         url: 'plan',
-        // abstract: true,
+        abstract: true, // 用中文明显点，这里其实个人觉得，abstract不应该用，用了会报错。
         views: {
           'tab-plan': {
             templateUrl: 'plan/tabs/tabs.html'
