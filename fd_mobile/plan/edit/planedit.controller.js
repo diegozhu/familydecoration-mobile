c101665297d57be83001c550512028b16419b1d2
alex123bob <alex123bob@126.com>
2017-03-24 12:38:19 +0800

    ng-repeat planItems

--- fd_mobile/plan/edit/planedit.controller.js
+++ fd_mobile/plan/edit/planedit.controller.js
@@ -1,6 +1,18 @@
 (function() {
   'use strict';
 
-  angular.module('fdmobile.plan.edit').controller('PlanEditController', function() {
+  angular.module('fdmobile.plan.edit').controller('PlanEditController', function(planItems, $log) {
+    var vm = this;
+
+    if (planItems.data) {
+      vm.planItems = planItems.data;
+    }
+    else {
+      planItems.then(function(res) {
+        vm.planItems = res.data;
+      }, function(res) {
+        $log.log(res.errMsg);
+      });
+    }
   });
 })();
